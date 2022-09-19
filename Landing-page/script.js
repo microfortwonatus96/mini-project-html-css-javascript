6a82d6cc548358922644dabfc44d64a41e6810ec
Micro Fortwonatus <microfortwonatus6@gmail.com>
2022-09-20 06:27:00 +0700

    update

--- Landing-page/script.js
+++ Landing-page/script.js
@@ -1,22 +1,31 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 100) {
-          $(".netflix-navbar").css("background" , "#0C0C0C");
-        }
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 100) {
+//           $(".netflix-navbar").css("background" , "#0C0C0C");
+//         }
   
-        else{
-            $(".netflix-navbar").css("background" , "transparent");  	
-        }
-    });
-  })
+//         else{
+//             $(".netflix-navbar").css("background" , "transparent");  	
+//         }
+//     });
+//   })
 
-  // const nav = document.querySelector('netflix-navbar');
+// Event scroll mengubah background-color navbar ketika di scroll
+let nav = document.querySelector('.netflix-navbar');
 
-  // window.addEventListener('scroll', () => {
-  //   if(window.scrollY >= 50){
-  //     nav.classList.add('.bg-sc');
-  //   }else{
-  //     nav.classList.add('.bg-trans');
-  //   }
-  // });
+  window.addEventListener('scroll', () => {
+    if(window.scrollY >= 50){
+      nav.classList.add('bg-sc');
+    }else{
+      nav.classList.remove('bg-sc');
+    }
+  });
+
+// innerHTML menambahkan tulisan 
+let title = document.querySelector('.title');
+  title.innerHTML = 'My Neighbour Totoro';
+
+// src menambahkan image
+let image = document.querySelector('.image');
+  image.src = "img/anime/anime7.webp";
